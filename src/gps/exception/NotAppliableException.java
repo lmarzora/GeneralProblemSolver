16f43e74c793cd3ec7e4d269c8cd5eebd8802220
Luis Marzoratti <lmarzora@itba.edu.ar>
2017-03-14 16:54:46 -0300

    mvn

--- src/gps/exception/NotAppliableException.java
+++
@@ -1,7 +0,0 @@
-package gps.exception;
-
-public class NotAppliableException extends Exception {
-
-	private static final long serialVersionUID = -4084061417773665019L;
-
-}
---
+++ src/main/java/gps/GPSNode.java
@@ -0,0 +1,63 @@
+package gps;
+
+import gps.api.GPSState;
+
+public class GPSNode {
+
+	private GPSState state;
+
+	private GPSNode parent;
+
+	private Integer cost;
+
+	public GPSNode(GPSState state, Integer cost) {
+		this.state = state;
+		this.cost = cost;
+	}
+
+	public GPSNode getParent() {
+		return parent;
+	}
+
+	public void setParent(GPSNode parent) {
+		this.parent = parent;
+	}
+
+	public GPSState getState() {
+		return state;
+	}
+
+	public Integer getCost() {
+		return cost;
+	}
+
+	@Override
+	public String toString() {
+		return state.toString();
+	}
+
+	public String getSolution() {
+		if (this.parent == null) {
+			return this.state.toString();
+		}
+		return this.parent.getSolution() + this.state.toString();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		GPSNode other = (GPSNode) obj;
+		if (state == null) {
+			if (other.state != null)
+				return false;
+		} else if (!state.equals(other.state))
+			return false;
+		return true;
+	}
+
+}
