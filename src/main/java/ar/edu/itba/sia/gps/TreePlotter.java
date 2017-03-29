57488c46662cb93c7127d37adc889018c643691e
Luis Marzoratti <lmarzora@itba.edu.ar>
2017-03-29 17:21:16 -0300

    DefaultEdge

--- src/main/java/ar/edu/itba/sia/gps/TreePlotter.java
+++ src/main/java/ar/edu/itba/sia/gps/TreePlotter.java
@@ -6,6 +6,7 @@ import org.jgraph.JGraph;
 import org.jgrapht.DirectedGraph;
 import org.jgrapht.ext.JGraphModelAdapter;
 import org.jgrapht.graph.DefaultDirectedGraph;
+import org.jgrapht.graph.DefaultEdge;
 
 
 import javax.swing.*;
@@ -13,19 +14,19 @@ import javax.swing.*;
 
 public class TreePlotter implements GPSObserver{
 
-    private DirectedGraph<GPSNode,GPSRule> graph;
+    private DirectedGraph<GPSNode,DefaultEdge> graph;
 
 
     @Override
     public void start(final GPSNode root) {
-        graph = new DefaultDirectedGraph<GPSNode, GPSRule>((node, v1) -> node.getGenerationRule());
+        graph = new DefaultDirectedGraph<GPSNode, DefaultEdge>(DefaultEdge.class);
         graph.addVertex(root);
     }
 
     @Override
     public void observe(final GPSNode node) {
         graph.addVertex(node);
-        graph.addEdge(node,node.getParent());
+        graph.addEdge(node.getParent(),node);
     }
 
     @Override
