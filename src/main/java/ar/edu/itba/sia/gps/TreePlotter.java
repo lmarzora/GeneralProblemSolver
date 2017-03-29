77553197f21d70c277a578a5fb1a1dd36684090f
Luis Marzoratti <lmarzora@itba.edu.ar>
2017-03-29 17:01:02 -0300

    repaint

--- src/main/java/ar/edu/itba/sia/gps/TreePlotter.java
+++ src/main/java/ar/edu/itba/sia/gps/TreePlotter.java
@@ -15,21 +15,25 @@ public class TreePlotter implements GPSObserver{
 
     private DirectedGraph<GPSNode,GPSRule> graph;
     private JGraph jgraph;
+    private JFrame frame;
+
     @Override
     public void start(final GPSNode root) {
         graph = new DefaultDirectedGraph<GPSNode, GPSRule>((node, v1) -> node.getGenerationRule());
-        JFrame frame = new JFrame();
+        frame = new JFrame();
         frame.setSize(400, 400);
         jgraph = new JGraph(new JGraphModelAdapter(graph));
         frame.getContentPane().add(jgraph);
         frame.setVisible(true);
         graph.addVertex(root);
+        frame.repaint();
     }
 
     @Override
     public void observe(final GPSNode node) {
         graph.addVertex(node);
         graph.addEdge(node,node.getParent());
+        frame.repaint();
     }
 
     @Override
