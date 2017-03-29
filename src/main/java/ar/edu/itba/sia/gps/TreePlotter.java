970c9dc719354633b8534d6df5928ffb688b100c
Luis Marzoratti <lmarzora@itba.edu.ar>
2017-03-29 17:14:13 -0300

    finalize plots

--- src/main/java/ar/edu/itba/sia/gps/TreePlotter.java
+++ src/main/java/ar/edu/itba/sia/gps/TreePlotter.java
@@ -14,30 +14,27 @@ import javax.swing.*;
 public class TreePlotter implements GPSObserver{
 
     private DirectedGraph<GPSNode,GPSRule> graph;
-    private JGraph jgraph;
-    private JFrame frame;
+
 
     @Override
     public void start(final GPSNode root) {
         graph = new DefaultDirectedGraph<GPSNode, GPSRule>((node, v1) -> node.getGenerationRule());
-        frame = new JFrame();
-        frame.setSize(400, 400);
-        jgraph = new JGraph(new JGraphModelAdapter(graph));
-        frame.getContentPane().add(jgraph);
-        frame.setVisible(true);
         graph.addVertex(root);
-        frame.repaint();
     }
 
     @Override
     public void observe(final GPSNode node) {
         graph.addVertex(node);
         graph.addEdge(node,node.getParent());
-        frame.repaint();
     }
 
     @Override
     public void finalize() {
+        JFrame frame = new JFrame();
+        frame.setSize(400, 400);
+        JGraph jgraph = new JGraph(new JGraphModelAdapter(graph));
+        frame.getContentPane().add(jgraph);
+        frame.setVisible(true);
 
     }
 }
