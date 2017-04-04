b7c10d4c37e1f31e7c0315996f914936b18ebfcf
Mati <mfraga@itba.edu.ar>
2017-04-04 09:57:28 -0300

    Contains strategy added

--- src/main/java/ar/edu/itba/sia/gps/SearchStrategy.java
+++ src/main/java/ar/edu/itba/sia/gps/SearchStrategy.java
@@ -2,4 +2,13 @@ package ar.edu.itba.sia.gps;
 
 public enum SearchStrategy {
     BFS, DFS, IDDFS, GREEDY, ASTAR;
+	
+	public static boolean contains(final String st) {
+
+	    for (SearchStrategy s : SearchStrategy.values()) {
+	        if (s.name().equals(st)) 
+	            return true;
+	    }
+	    return false;
+	}
 }
